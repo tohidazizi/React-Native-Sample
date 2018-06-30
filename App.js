b0f46a77a21daba6b5579e7f3d73e9571fcc5a38
Tohid Azizi <tohidazizi@outlook.com>
2018-06-30 14:31:39 +0430

    Add App Center’s SDK to your app.

--- App.js
+++ App.js
@@ -8,8 +8,12 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Text>Hello. This is Tohid!!!</Text>
+        </View>
       </View>
-    );
+
+);
   }
 }
 
